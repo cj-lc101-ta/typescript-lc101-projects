d3e789849c0074cb4bda0d0515f69325d3c2583f
cjconnor18 <cjallen1988@gmail.com>
2020-09-02 14:39:00 -0500

    created Astronaut class

--- studio/Astronaut.ts
+++ studio/Astronaut.ts
@@ -1,3 +1,10 @@
-export class Astronaut {
-  
+import {Payload} from './Payload';
+
+export class Astronaut implements Payload{
+  massKg:number;
+  name:string;
+  constructor(massKg:number, name:string){
+    this.name = name;
+    this.massKg = massKg;
+  }
 }
